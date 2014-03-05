b35a3ff1be96706e6a30eb6a4b1e3b8a3c03eef4
Jen <jenniferballing@gmail.com>
2014-03-05 16:57:07 -0700

    Getting the file to read into function

--- CS2Assignment6/main.cpp
+++ CS2Assignment6/main.cpp
@@ -10,32 +10,61 @@ int main()
    
     while (environ[k] != NULL)
     {
-        cout << environ[k] << endl;
+        //cout << environ[k] << endl;
         k++;
     }
-    cout<<endl;
+    //cout<<"K: "<<k<<endl;
 
     wordSearch (environ);
 
     return 0;
 }
-void wordSearch(extern char** e)
+void wordSearch(extern char** e)//create an array of words to work with
 {
-   char* temp[45]={0, 0};
-   int i=0;
-   char* word="jen";
-   
-   for(i=0; i<45; i++)
+   //cout<<e[0]<<endl<<endl;
+   //cout<<*(e+40)<<endl<<endl;
+   //cout<<*(*e)<<endl<<endl;
+   int i=0, count=0;
+   string str=" ",line;//, line2, line3, line4;
+   //char *tr=e;
+   int len= strlen(*e);
+   //cout<<"LEN: "<<len<<endl;
+
+   int counter=0;
+   //cout<<"LEN: "<<endl;
+   for(i=0; i<44; i++)
    {
-       temp[i]=e[i];
-       string str;
-       str+= temp[i];
-       cout<<str<<endl;
-       if(strcmp(word, temp[i])!=0)
-        {
-            cout<<"Found a match!"<<endl;
-        }
+       str+= *(e+i);
+       cout<<str<<endl<<endl;
+       counter++;
    }
+  // cout<<"Counter: "<<counter<<endl;
+   //cout<<"Before SS: "<<str<<endl;
+   stringstream ss (str);
+   //cout<<endl<<endl<<"After SS: "<<str<<endl;//doesn't have all of the file
    
 
+   //CHECKING FOR POSSIBLE WORDS
+         
+   getline(ss, line);//possibly reading whole file without error
+   int len2= line.length();
+   string arr[3000];
+   cout<<"LEN2: "<<len2<<endl;
+   
+       for(int i=0; i<len2; i++)
+       {
+           
+            if(isalnum(line[i])||line[i]=='_')
+            {
+                arr[i]=line[i];
+            }
+            else
+            {
+                arr[i]=' ';
+            }
+           // cout<<arr[i];
+            
+            count++;
+       }
+     //  cout<<endl<<"COUNT:"<<count<<endl;
 }
