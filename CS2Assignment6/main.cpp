235fef395955537a517f5c32f831990ff1c594d1
Jen <jenniferballing@gmail.com>
2014-03-08 16:47:41 -0700

    Yay! The words are read correctly

--- CS2Assignment6/main.cpp
+++ CS2Assignment6/main.cpp
@@ -37,21 +37,19 @@ void wordSearch(char** e)//create an array of words to work with
    while(*e!=NULL)
    {
        lines.insert(lines.end(), *(e));
-       //str+= *(e++);
+       str+= *(e++);
    }
-   str+= *(e++);
    str+=" ";
-   
    string word;
    int j=0;
    int alpha=0;
-    int i=0;
-    for(i=0; i<str.length(); i++)
-    {
+   int i=0;
+   for(i=0; i<str.length(); i++)
+   {
         char temp=str[i];
-        if((temp>0 && temp <48)|| (temp<65 && temp>57) || (temp>122))//ELIMINATE ILLEGAL CHARS
+        if(!isalpha (temp) && !isdigit(temp) && temp!='_')//(temp>0&&temp<65) || (temp<90 && temp< 97 ) || (temp >122))//ELIMINATE ILLEGAL CHARS
         {
-            temp=' ';
+            temp= ' ';
         }
         if(temp!=' ')
         {
@@ -63,18 +61,31 @@ void wordSearch(char** e)//create an array of words to work with
         }            
         if(temp == ' ')
         {
-            if(word.length()>2 && alpha>0)//LONG ENOUGH WITH ENOUGH LETTERS
+            if(word.length()>1 && alpha>0)//LONG ENOUGH WITH ENOUGH LETTERS
             {
                 alpha=0;
                 words.insert(words.end(), word);
                 word.clear();
-                for(vector<string>::const_iterator k = words.begin(); k != words.end(); ++k)
+                /*for(vector<string>::const_iterator k = words.begin(); k != words.end(); ++k)
                 {
                     cout << *k <<" " ;
-                }
+                }*/
+            }
+            else
+            {
+                word.clear();
             }
         }                 
     }
    cout<<"TOTAL WORDS: "<<(int) words.size()<<endl;
+   int jen=0, levi=0;
+
+   for(jen=0; j<words.size(); jen++)
+    {
+        for(levi=0; levi<words.size()-1; levi++)
+        {
+
+        }
+    }
 
 }
